11ad0db0d46f40557d57d1f23d6ecd2eb9e094e7
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-07-21 18:08:34 -0300

    include dataTable in container

--- src/containers/TableContainer.tsx
+++ src/containers/TableContainer.tsx
@@ -13,8 +13,20 @@ import { useQuery, useQueryClient } from "react-query";
 import getQuery from "../utils/getQuery";
 import { getData } from "../services/HttpService";
 import SearchBar from "../components/SearchBar";
+import DataTable from "../components/DataTable";
+import Commerce from "../models/Commerce.model";
 
 const ROWS_PER_PAGE = 10;
+const fields: Array<Array<{ key: keyof Commerce; name: string }>> = [
+  [
+    { key: "id", name: "ID" },
+    { key: "name", name: "Comercio" },
+    { key: "cuit", name: "CUIT" },
+    { key: "currentBalance", name: "Balance Actual" },
+    { key: "active", name: "Activo" },
+    { key: "lastSale", name: "Última Venta" },
+  ]
+]
 
 export default function TableContainer() {
   const [searchData, setSearchData] = useState<string>("");
@@ -49,6 +61,7 @@ export default function TableContainer() {
       px={{ base: "10px" }}
     >
       <SearchBar onSearch={setSearchData} />
+      <DataTable data={data.data} fields={fields[0]} />
     </Box>
   );
 }
