36c5077c6fccd090a0e98c4c4611d02694331021
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-07-21 18:23:04 -0300

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Box, Divider, Heading } from '@chakra-ui/react';
 import React from 'react';
 import './App.css';
+import TableContainer from './containers/TableContainer';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return (<>
+    <Box pb={4}>
+      <Heading as="h1" textAlign="center" my={5} >Datos de Comercios</Heading>
+      <Divider />
+      <TableContainer />
+    </Box>
+  </>);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { worker } from "./mocks/browser";
 
 const queryClient = new QueryClient();
+
+worker.start();
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <QueryClientProvider client={queryClient} >
+      <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
