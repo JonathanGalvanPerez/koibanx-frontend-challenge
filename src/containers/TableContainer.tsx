df3a898c125b83b05887e3f95d5b6b9d8c8e37d1
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-07-21 18:03:51 -0300

    config react query

--- src/containers/TableContainer.tsx
+++ src/containers/TableContainer.tsx
@@ -9,14 +9,38 @@ import {
   TabPanels,
   Tabs,
 } from "@chakra-ui/react";
+import { useQuery, useQueryClient } from "react-query";
+import getQuery from "../utils/getQuery";
+import { getData } from "../services/HttpService";
 import SearchBar from "../components/SearchBar";
 
+const ROWS_PER_PAGE = 10;
 
 export default function TableContainer() {
   const [searchData, setSearchData] = useState<string>("");
   const [activeFilter, setActiveFilter] = useState<string>("");
   const [order, setOrder] = useState<string>("id");
   const [page, setPage] = useState<number>(1);
+  const queryClient = useQueryClient();
+  const { data, isPreviousData } = useQuery(
+    ["projects", searchData, activeFilter, order, page],
+    () =>
+      getData(getQuery(searchData, activeFilter, order, page, ROWS_PER_PAGE)),
+    { keepPreviousData: true, staleTime: 5000 }
+  );
+
+  useEffect(() => {
+    console.log("Re fetching...");
+
+    queryClient.prefetchQuery(
+      ["projects", searchData, activeFilter, order, page + 1],
+      () =>
+        getData(
+          getQuery(searchData, activeFilter, order, page + 1, ROWS_PER_PAGE)
+        )
+    );
+  }, [activeFilter, order, page, queryClient, searchData]);
+
 
   return (
     <Box
