716b7aaf58dc4a4864e01810aa3bb307e7d21a59
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-07-21 17:47:25 -0300

    add commerce model

---
+++ src/models/Commerce.model.ts
@@ -0,0 +1,14 @@
+export default interface Commerce {
+  id: string,
+  name: string,
+  cuit: string,
+  concept1: number,
+  concept2: number,
+  concept3: number,
+  concept4: number,
+  concept5: number,
+  concept6: number,
+  currentBalance: number,
+  active: string,
+  lastSale: string,
+}
